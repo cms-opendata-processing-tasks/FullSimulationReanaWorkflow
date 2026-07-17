f90682e6ef33a5dde98773d685062aeabeb8ebc4
Khaled Abu Qutish <abuqutishkhaled@gmail.com>
2026-07-17 11:46:28 +0200

    automating the sim and the predigi2raw rules

--- workflow/scripts/parse_metadata.py
+++ workflow/scripts/parse_metadata.py
@@ -6,24 +6,31 @@ import subprocess
 
 metadata_json = sys.argv[1]
 cmsDriver_command_path = sys.argv[2]
+step = sys.argv[3]
+get_curl = sys.argv[4]
 
 with open(metadata_json, "r") as f:
     metadata = json.load(f)
 
-script_content = metadata["methodology"]["steps"][0]["configuration_files"][0]["script"]
+map_step = {
+    "gen": 0,
+    "sim": 1
+}
 
+script_content = metadata["methodology"]["steps"][map_step[step]]["configuration_files"][0]["script"]
 
-curl_pattern = r"(curl.*? -o\s+(\S+))"
-curl_match = re.search(curl_pattern, script_content)
-if curl_match:
-    curl_command = curl_match.group(1)
-    print(curl_command)
-else:
-    raise ValueError("Fragment URL not found")
+if get_curl.lower() == "true":
+    curl_pattern = r"(curl.*? -o\s+(\S+))"
+    curl_match = re.search(curl_pattern, script_content)
+    if curl_match:
+        curl_command = curl_match.group(1)
+        print(curl_command)
+    else:
+        raise ValueError("Fragment URL not found")
 
-subprocess.run(curl_command, shell=True)
+    subprocess.run(curl_command, shell=True)
 
-cmsDriver_pattern = "cmsDriver\.py.*?-n\s+\S+"
+cmsDriver_pattern = r"cmsDriver\.py.*?-n\s+\S+"
 cmsDriver_match = re.search(cmsDriver_pattern, script_content, re.DOTALL)
 if cmsDriver_match:
     print(cmsDriver_match.group(0))
@@ -32,8 +39,17 @@ else:
     raise ValueError("cmsDriver.py command not found")
 
 cmsDriver_snakemake = cmsDriver.replace("--customise Configuration/DataProcessing/Utils.addMonitoring", "")
-cmsDriver_snakemake = re.sub(r"--python_filename\s+\S+", "--python_filename gen_cfg_${JOB_INDEX}.py", cmsDriver_snakemake)
-cmsDriver_snakemake = re.sub(r"--fileout\s+file:\S+", "--fileout file:gen_output.root", cmsDriver_snakemake)
+cmsDriver_snakemake = re.sub(r"--python_filename\s+\S+", f"--python_filename {step}_cfg_${{JOB_INDEX}}.py", cmsDriver_snakemake)
+cmsDriver_snakemake = re.sub(r"--fileout\s+file:\S+", f"--fileout file:{step}_output.root", cmsDriver_snakemake)
+
+if step == "sim":
+    cmsDriver_snakemake = re.sub(r"--filein\s+file:\S+", "--filein file:${INPUT_FILE}", cmsDriver_snakemake)
+
+    # get the pileup recordId
+    print(metadata['pileup'])
+    pileup_record_id = metadata['pileup']['links'][0]['recid']
+    with open("pileup_record_id.txt", 'w') as f:
+        f.write(pileup_record_id)
 
 with open(cmsDriver_command_path, "w") as f:
     f.write(cmsDriver_snakemake)
