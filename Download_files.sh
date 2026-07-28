f87c495d61ee46dbd756bc2abc3b9f2a5bef68ac
Tarshihi <164808523+Tarshihi@users.noreply.github.com>
2026-07-28 15:17:00 +0300

    Moved Download_files to Scripts

--- Download_files.sh
+++
@@ -1,9 +0,0 @@
-read -p "Enter workflow name: " workflow
-
-for file in $(reana-client ls -w "$workflow" | \
-              grep -v NAME | \
-              awk '{print $1}' | \
-              grep '^results/nano_.*\.root$')
-do
-    reana-client download -w "$workflow" "$file"
-done
