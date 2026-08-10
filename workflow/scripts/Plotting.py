688d0a7d6c8146365c488b6d8d9663446b6d9cef
Khaled Abu Qutish <abuqutishkhaled@gmail.com>
2026-08-10 16:20:02 +0200

    changed plotting to a usable function

--- workflow/scripts/Plotting.py
+++ workflow/scripts/Plotting.py
@@ -1,23 +1,84 @@
 import uproot
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 import awkward as ak
 import numpy as np
 
-file = uproot.open("/afs/cern.ch/user/m/mtarshih/FullSimulationReanaWorkflow/results/nano_merged.root:Events")
-flattened_electron_pt = ak.flatten(file['Electron_pt'].array())
+file = uproot.open("/eos/user/k/kabuquti/1000_events_unique/results/nano_merged.root:Events")
 
-plt.hist(
-    flattened_electron_pt, 
+electron_pt  = ak.flatten(file['Electron_pt'].array())
+electron_phi = ak.flatten(file['Electron_phi'].array())
+electron_eta = ak.flatten(file['Electron_eta'].array())
+
+met_pt    = file['MET_pt'].array()
+nElectron = file['nElectron'].array()
+
+def plot_histogram(data, bins, x_range, title, xlabel, ylabel, filename):
+
+    plt.figure(figsize=(8, 6))
+    
+    plt.hist(
+        data, 
+        bins=bins, 
+        range=x_range, 
+        color='#0b57d0', 
+        alpha=0.8, 
+        edgecolor='black'
+    )
+
+    plt.title(title, fontsize=14, fontweight='bold')
+    plt.xlabel(xlabel, fontsize=12)
+    plt.ylabel(ylabel, fontsize=12)
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+
+    plt.savefig(filename)
+    plt.close()
+
+plot_histogram(
+    data=electron_pt, 
     bins=50, 
-    range=(0, 150), 
-    color='#0b57d0', 
-    alpha=0.8, 
-    edgecolor='black'
+    x_range=(0, 150), 
+    title="Electron Transverse Momentum ($p_T$)", 
+    xlabel="Electron $p_T$ (GeV)", 
+    ylabel="Number of Electrons",
+    filename="plots/plot_1_pt.png"
 )
 
-plt.title("Electron Transverse Momentum ($p_T$)", fontsize=14, fontweight='bold')
-plt.xlabel("Electron $p_T$ (GeV)", fontsize=12)
-plt.ylabel("Number of Electrons", fontsize=12)
-plt.grid(axis='y', linestyle='--', alpha=0.7)
+plot_histogram(
+    data=electron_phi, 
+    bins=50, 
+    x_range=(-3.2, 3.2), 
+    title="Electron Azimuthal Angle ($\phi$)", 
+    xlabel="Electron $\phi$ (rad)", 
+    ylabel="Number of Electrons",
+    filename="plots/plot_2_phi.png"
+)
 
-plt.savefig('Final_Plot.png')
+plot_histogram(
+    data=electron_eta, 
+    bins=50, 
+    x_range=(-3.0, 3.0), 
+    title="Electron Pseudorapidity ($\eta$)", 
+    xlabel="Electron $\eta$", 
+    ylabel="Number of Electrons",
+    filename="plots/plot_3_eta.png"
+)
+
+plot_histogram(
+    data=met_pt, 
+    bins=50, 
+    x_range=(0, 200), 
+    title="Missing Transverse Energy (MET)", 
+    xlabel="MET (GeV)", 
+    ylabel="Number of Events",
+    filename="plots/plot_4_met.png"
+)
+
+plot_histogram(
+    data=nElectron, 
+    bins=10, 
+    x_range=(0, 10), 
+    title="Electron Multiplicity per Event", 
+    xlabel="Number of Electrons", 
+    ylabel="Number of Events",
+    filename="plots/plot_5_nelectron.png"
+)
