095595a8575b98427c72f7857ae913e16ea6b176
Mohammad Tarshihi <muhammadtarshihi@gmail.com>
2026-08-19 15:37:28 +0200

    Automated Statistical comparison & finalized plotting

--- workflow/scripts/Plotting.py
+++ workflow/scripts/Plotting.py
@@ -2,15 +2,40 @@ import uproot
 import matplotlib.pyplot as plt
 import awkward as ak
 import numpy as np
+import os 
 
-file = uproot.open("/eos/user/k/kabuquti/1000_events_unique/results/nano_merged.root:Events")
+file = uproot.open("/eos/user/m/mtarshih/my_project/results/nano_merged.root:Events")
+#Insert your own EOS directory where nano_merged.root exists
 
-electron_pt  = ak.flatten(file['Electron_pt'].array())
-electron_phi = ak.flatten(file['Electron_phi'].array())
-electron_eta = ak.flatten(file['Electron_eta'].array())
+os.makedirs("plots", exist_ok=True)
 
-met_pt    = file['MET_pt'].array()
-nElectron = file['nElectron'].array()
+nElectron = ak.to_numpy(file['nElectron'].array())
+electron_pt  = ak.to_numpy(ak.flatten(file['Electron_pt'].array()))
+electron_phi = ak.to_numpy(ak.flatten(file['Electron_phi'].array()))
+electron_eta = ak.to_numpy(ak.flatten(file['Electron_eta'].array()))
+
+nMuon = ak.to_numpy(file['nMuon'].array())
+muon_pt  = ak.to_numpy(ak.flatten(file['Muon_pt'].array()))
+muon_phi = ak.to_numpy(ak.flatten(file['Muon_phi'].array()))
+muon_eta = ak.to_numpy(ak.flatten(file['Muon_eta'].array()))
+
+met_phi = ak.to_numpy(file['MET_phi'].array())
+met_pt    = ak.to_numpy(file['MET_pt'].array())
+
+nJet = ak.to_numpy(file['nJet'].array())
+jet_pt  = ak.to_numpy(ak.flatten(file['Jet_pt'].array()))
+jet_phi = ak.to_numpy(ak.flatten(file['Jet_phi'].array()))
+jet_eta = ak.to_numpy(ak.flatten(file['Jet_eta'].array()))
+
+nPhoton = ak.to_numpy(file['nPhoton'].array())
+photon_pt  = ak.to_numpy(ak.flatten(file['Photon_pt'].array()))
+photon_phi = ak.to_numpy(ak.flatten(file['Photon_phi'].array()))
+photon_eta = ak.to_numpy(ak.flatten(file['Photon_eta'].array()))
+
+nTau = ak.to_numpy(file['nTau'].array())
+tau_pt  = ak.to_numpy(ak.flatten(file['Tau_pt'].array()))
+tau_phi = ak.to_numpy(ak.flatten(file['Tau_phi'].array()))
+tau_eta = ak.to_numpy(ak.flatten(file['Tau_eta'].array()))
 
 def plot_histogram(data, bins, x_range, title, xlabel, ylabel, filename):
 
@@ -46,9 +71,9 @@ plot_histogram(
 plot_histogram(
     data=electron_phi, 
     bins=50, 
-    x_range=(-3.2, 3.2), 
-    title="Electron Azimuthal Angle ($\phi$)", 
-    xlabel="Electron $\phi$ (rad)", 
+    x_range=(-3.15, 3.15), 
+    title=r"Electron Azimuthal Angle ($\phi$)", 
+    xlabel=r"Electron $\phi$ (rad)", 
     ylabel="Number of Electrons",
     filename="plots/plot_2_phi.png"
 )
@@ -57,8 +82,8 @@ plot_histogram(
     data=electron_eta, 
     bins=50, 
     x_range=(-3.0, 3.0), 
-    title="Electron Pseudorapidity ($\eta$)", 
-    xlabel="Electron $\eta$", 
+    title=r"Electron Pseudorapidity ($\eta$)", 
+    xlabel=r"Electron $\eta$", 
     ylabel="Number of Electrons",
     filename="plots/plot_3_eta.png"
 )
@@ -81,4 +106,174 @@ plot_histogram(
     xlabel="Number of Electrons", 
     ylabel="Number of Events",
     filename="plots/plot_5_nelectron.png"
+)
+
+plot_histogram(
+    data=nMuon, 
+    bins=10, 
+    x_range=(0, 10), 
+    title="Muon Multiplicity per Event", 
+    xlabel="Number of Muons", 
+    ylabel="Number of Events",
+    filename="plots/plot_6_nmuon.png"
+)
+
+plot_histogram(
+    data=muon_pt, 
+    bins=50, 
+    x_range=(0, 150), 
+    title="Muon Transverse Momentum ($p_T$)", 
+    xlabel="Muon $p_T$ (GeV)", 
+    ylabel="Number of Muons",
+    filename="plots/plot_7_pt.png"
+)
+
+plot_histogram(
+    data=muon_phi, 
+    bins=50, 
+    x_range=(-3.15, 3.15), 
+    title=r"Muon Azimuthal Angle ($\phi$)", 
+    xlabel=r"Muon $\phi$ (rad)", 
+    ylabel="Number of Muons",
+    filename="plots/plot_8_phi.png"
+)
+
+plot_histogram(
+    data=muon_eta, 
+    bins=50, 
+    x_range=(-3.15, 3.15), 
+    title=r"Muon Pseudorapidity ($\eta$)", 
+    xlabel=r"Muon $\eta$", 
+    ylabel="Number of Muons",
+    filename="plots/plot_9_eta.png"
+)
+
+plot_histogram(
+    data=met_phi, 
+    bins=50, 
+    x_range=(-3.15, 3.15), 
+    title=r"Missing Transverse Energy Azimuthal Angle ($\phi$)", 
+    xlabel=r"MET $\phi$ (rad)", 
+    ylabel="Number of Events",
+    filename="plots/plot_10_met_phi.png"
+)
+
+plot_histogram(
+    data=nJet, 
+    bins=20, 
+    x_range=(0, 15), 
+    title="Jet Multiplicity per Event", 
+    xlabel="Number of Jets", 
+    ylabel="Number of Events",
+    filename="plots/plot_11_njet.png"
+)
+
+plot_histogram(
+    data=jet_pt, 
+    bins=50, 
+    x_range=(0, 150), 
+    title="Jet Transverse Momentum ($p_T$)", 
+    xlabel="Jet $p_T$ (GeV)", 
+    ylabel="Number of Jets",
+    filename="plots/plot_12_pt.png"
+)
+
+plot_histogram(
+    data=jet_phi, 
+    bins=50, 
+    x_range=(-3.15, 3.15), 
+    title=r"Jet Azimuthal Angle ($\phi$)", 
+    xlabel=r"Jet $\phi$ (rad)", 
+    ylabel="Number of Jets",
+    filename="plots/plot_13_phi.png"
+)
+
+plot_histogram(
+    data=jet_eta, 
+    bins=50, 
+    x_range=(-6, 6), 
+    title=r"Jet Pseudorapidity ($\eta$)", 
+    xlabel=r"Jet $\eta$", 
+    ylabel="Number of Jets",
+    filename="plots/plot_14_eta.png"
+)
+
+plot_histogram(
+    data=nPhoton, 
+    bins=20, 
+    x_range=(0, 10), 
+    title="Photon Multiplicity per Event", 
+    xlabel="Number of Photons", 
+    ylabel="Number of Events",
+    filename="plots/plot_15_nphoton.png"
+)
+
+plot_histogram(
+    data=photon_pt, 
+    bins=50, 
+    x_range=(0, 150), 
+    title="Photon Transverse Momentum ($p_T$)", 
+    xlabel="Photon $p_T$ (GeV)", 
+    ylabel="Number of Photons",
+    filename="plots/plot_16_pt.png"
+)
+
+plot_histogram(
+    data=photon_phi, 
+    bins=50, 
+    x_range=(-3.15, 3.15), 
+    title=r"Photon Azimuthal Angle ($\phi$)", 
+    xlabel=r"Photon $\phi$ (rad)", 
+    ylabel="Number of Photons",
+    filename="plots/plot_17_phi.png"
+)
+
+plot_histogram(
+    data=photon_eta, 
+    bins=50, 
+    x_range=(-3.0, 3.0), 
+    title=r"Photon Pseudorapidity ($\eta$)", 
+    xlabel=r"Photon $\eta$", 
+    ylabel="Number of Photons",
+    filename="plots/plot_18_eta.png"
+)
+
+plot_histogram(
+    data=nTau, 
+    bins=20, 
+    x_range=(0, 10), 
+    title="Tau Multiplicity per Event", 
+    xlabel="Number of Taus", 
+    ylabel="Number of Events",
+    filename="plots/plot_19_ntau.png"
+)
+
+plot_histogram(
+    data=tau_pt, 
+    bins=50, 
+    x_range=(0, 150), 
+    title="Tau Transverse Momentum ($p_T$)", 
+    xlabel="Tau $p_T$ (GeV)", 
+    ylabel="Number of Taus",
+    filename="plots/plot_20_pt.png"
+)
+
+plot_histogram(
+    data=tau_phi, 
+    bins=50, 
+    x_range=(-3.15, 3.15), 
+    title=r"Tau Azimuthal Angle ($\phi$)", 
+    xlabel=r"Tau $\phi$ (rad)", 
+    ylabel="Number of Taus",
+    filename="plots/plot_21_phi.png"
+)
+
+plot_histogram(
+    data=tau_eta, 
+    bins=50, 
+    x_range=(-3.0, 3.0), 
+    title=r"Tau Pseudorapidity ($\eta$)", 
+    xlabel=r"Tau $\eta$", 
+    ylabel="Number of Taus",
+    filename="plots/plot_22_eta.png"
 )
--- workflow/scripts/Stat_comparison.py
+++ workflow/scripts/Stat_comparison.py
@@ -4,6 +4,12 @@ import numpy as np
 import pandas as pd 
 import boost_histogram as bh
 from scipy.stats import chi2 as chi2_dist
+import sys 
+
+merged_nano = sys.argv[1]
+reference_root = sys.argv[2]
+comparison_report = sys.argv[3]
+
 
 def load_variable(filepath, tree_name, branch_name):
     with uproot.open(filepath) as f:
@@ -33,25 +39,28 @@ variable_configs = [
     ("Muon_pt", "Muon_pt", 50, (0, 150)),
     ("Electron_phi", "Electron_phi", 50, (-3.15, 3.15)),
     ("Muon_phi", "Muon_phi", 50, (-3.15, 3.15)),
-    ("Electron_eta", "Electron_eta", 50, (-2.5, 2.5)),
-    ("Muon_eta", "Muon_eta", 50, (-3.15, 3.15))
+    ("Electron_eta", "Electron_eta", 50, (-3, 3)),
+    ("Muon_eta", "Muon_eta", 50, (-3.15, 3.15)),
+    ("MET_phi", "MET_phi", 50, (-3.15, 3.15)),
+    ("MET_pt", "MET_pt", 50, (0, 200)),
+    ("nJet", "nJet", 20, (0, 15)),
+    ("Jet_pt", "Jet_pt", 50, (0, 150)),
+    ("Jet_phi", "Jet_phi", 50, (-3.15, 3.15)),
+    ("Jet_eta", "Jet_eta", 50, (-6, 6)),
+    ("nPhoton", "nPhoton", 20, (0, 10)),
+    ("Photon_pt", "Photon_pt", 50, (0, 150)),
+    ("Photon_phi", "Photon_phi", 50, (-3.15, 3.15)),
+    ("Photon_eta", "Photon_eta", 50, (-3, 3)),
+    ("nTau", "nTau", 20, (0, 10)),
+    ("Tau_pt", "Tau_pt", 50, (0, 150)),
+    ("Tau_phi", "Tau_phi", 50, (-3.15, 3.15)),
+    ("Tau_eta", "Tau_eta", 50, (-3, 3)),
 ]
-
-
-'''
-The Stat_comparison.py script still needs a few updates:
-1) Variable_configs needs to be changed/completely altered depending on the workflow we're simulating (different particles being generated)
-2) The downloaded ROOT file from CERN Open Data should also differ depending on the workflow itself, and it's unclear whether multiple runs of the same workflow 
-entails comparisons with different ROOT files 
-3) Perhaps depending on the size of the NanoAOD file, it could either be downloaded
-locally automatically and then loaded using load_variable() or if it exceeds a certain file size,
-it could be streamed in some way 
-'''
 
 results = []
 for var, tree_branch, bins, range_ in variable_configs:
-    sim = load_variable("Fixed_nano_merge.root", "Events", tree_branch) #Our generated NanoAOD file
-    data = load_variable("873EB063-D8FB-1441-9993-D78CEE0F7D5E.root", "Events", tree_branch) #The downloaded ROOT file we're comparing it to
+    sim = load_variable(merged_nano, "Events", tree_branch) #Our generated NanoAOD file
+    data = load_variable(reference_root, "Events", tree_branch)
     n_sim, n_data, edges = make_comparable_histograms(sim, data, bins, range_)
     chi2_val, ndof = two_sample_chi2(n_sim, n_data)
     p = chi2_dist.sf(chi2_val, ndof)
@@ -63,4 +72,5 @@ for var, tree_branch, bins, range_ in variable_configs:
     })
 
 df = pd.DataFrame(results)
-print(df)
+
+df.to_csv(comparison_report, float_format="%.3f", index=False)
